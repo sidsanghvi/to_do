673f07bacaca9611d5291c7fd664efc667a97b07
Sid Sanghvi <sidsanghvi7@gmail.com>
2020-07-21 17:02:16 -0500

    added secrets

---
+++ todo/secrets.py
@@ -0,0 +1,1 @@
+django_secret_key = '-r=w)8d#m10h(mcm+r@$1q69!7ef8j6llr-uo&1v*ok9p-@(=)'
--- todo/settings.py
+++ todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-r=w)8d#m10h(mcm+r@$1q69!7ef8j6llr-uo&1v*ok9p-@(=)'
+SECRET_KEY = django_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
